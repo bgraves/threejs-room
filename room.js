99790488dcdb9562d5000d98e32c8b93570f4d58
JBG <jamesbryangraves@gmail.com>
2014-08-28 22:34:02 +0200

    clean up

---
+++ room.js
@@ -0,0 +1,217 @@
+var camera, scene, renderer;
+var controls;
+var cube;
+var min = 100, max = 1000, norm = 400;
+var width = norm, height = norm, depth = norm;
+
+var maxRise = height/2-20, minRise = -(height/2-30);
+
+var y = 25;
+var z = 25;
+
+var narrow = false;
+var tall = false;
+var cylinder = false;
+var rise = 0;
+
+init();
+animate();
+
+function init() {
+  renderer = new THREE.WebGLRenderer();
+  renderer.setClearColor( 0xfffa00, 1);
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  //
+  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
+  camera.position.x = 0;
+  camera.position.y = y;
+  camera.position.z = z;
+  scene = new THREE.Scene();
+
+  controls = new THREE.TrackballControls( camera );
+
+  controls.rotateSpeed = 1.0;
+  controls.zoomSpeed = 1.2;
+  controls.panSpeed = 0.8;
+
+  controls.noZoom = false;
+  controls.noPan = false;
+
+  controls.staticMoving = true;
+  controls.dynamicDampingFactor = 0.3;
+
+  controls.keys = [ 65, 83, 68 ];
+
+  controls.addEventListener( 'change', renderer );
+
+  // Tempory state change - JBG
+  window.addEventListener("keydown", function(e) {
+    if (e.keyCode == 87) {
+      narrow = !narrow;
+    } else if(e.keyCode == 72) {
+      tall = !tall;
+    } else if(e.keyCode == 67) {
+      cylinder = !cylinder;
+    } else if(e.keyCode == 82) {
+      rise++;
+    }
+  });
+
+  //var geometry = new THREE.BoxGeometry( 200, 200, 200 );
+  //var texture = THREE.ImageUtils.loadTexture( 'textures/crate.gif' );
+  //texture.anisotropy = renderer.getMaxAnisotropy();
+  //var material = new THREE.MeshBasicMaterial( { map: texture } );
+  //mesh = new THREE.Mesh( geometry, material );
+  //scene.add( mesh );
+
+  /*
+  var materials = [];
+  for(var i = 0; i < 6; i++) {
+    var img = new Image();
+    img.src = i + '.png';
+    var tex = new THREE.Texture(img);
+    img.tex = tex;
+    img.onload = function() {
+      this.tex.needsUpdate = true;
+    };
+    var mat = new THREE.MeshBasicMaterial({color: 0xffffff, map: tex});
+    var mat = new THREE.MeshBasicMaterial({ wireframe: true, color: 'blue' });
+    materials.push(mat);
+  }
+  */
+
+  //var cubeGeo = new THREE.BoxGeometry(200,200,200);
+  //cube = new THREE.Mesh(cubeGeo, new THREE.MeshBasicMaterial());
+
+  var img = new Image();
+  img.src = 'Grid.png';
+  var tex = new THREE.Texture(img);
+  img.tex = tex;
+  img.onload = function() {
+    this.tex.needsUpdate = true;
+  };
+  var material = new THREE.MeshBasicMaterial({color: 0xffffff, map: tex});
+
+        
+  /*
+  materials = [
+    //new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true, transparent: true, opacity: 0.1 } ),
+    //new THREE.MeshBasicMaterial( { color: 0x0000ff, wireframe: true, transparent: true, opacity: 0.1 } ),
+    //new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true, transparent: true, opacity: 0.1 } )
+    new THREE.MeshBasicMaterial( { color: 0xffffff } ),
+    new THREE.MeshBasicMaterial( { color: 0xffffff } ),
+    new THREE.MeshBasicMaterial( { color: 0xffffff } ),
+    new THREE.MeshBasicMaterial( { color: 0xffffff } ),
+    new THREE.MeshBasicMaterial( { color: 0xffffff } ),
+    new THREE.MeshBasicMaterial( { color: 0xffffff } )
+  ];
+  */
+  materials = [
+     //new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide, transparent: true, opacity: 0.0 }),
+     //new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide, transparent: true, opacity: 0.0 }),
+     //new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide, transparent: true, opacity: 0.0 }),
+     new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide }),
+     new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide }),
+     new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide }),
+     new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide }),
+     new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide }),
+     new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide }),
+     //new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide, transparent: true, opacity: 0.0 }),
+  ];
+
+  //var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  //material.side = THREE.BackSide;
+  //cube = new THREE.Mesh(new THREE.BoxGeometry(400, 400, 400), material);
+
+  cube = new THREE.Mesh(new THREE.BoxGeometry( width, height, depth ),  new THREE.MeshFaceMaterial( materials ));
+  //cube = new THREE.EdgesHelper( mesh, 0x000000 );
+  //cube.material.linewidth = 2;
+  //cube = THREE.SceneUtils.createMultiMaterialObject( new THREE.SphereGeometry( 75, 20, 10 ), materials );
+  cube.position.set( 0, y, 0 );
+  scene.add( cube );
+
+  //var edges = new THREE.WireframeHelper(cube, 0x000000);
+  //edges.material.linewidth = 3;
+  //scene.add(edges)
+
+  //cube.rotation.x = Math.PI * 0.1;
+  //scene.add( cube );
+
+  window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  controls.handleResize();
+  renderer.render( scene, camera );
+}
+
+function animate() {
+  requestAnimationFrame( animate );
+  checkStates();
+  //cube.rotation.x += 0.005;
+  cube.rotation.y += 0.01;
+  renderer.render( scene, camera );
+  controls.update();
+}
+
+function checkStates() {
+
+  // Get skinnier - JBG
+  if(narrow && width > min) {
+    width -= 1;
+  } else if(!narrow && width < norm) {
+    width += 1;
+  }
+
+  // Get shorter - JBG`
+  if(tall && height > min) {
+    height -= 1;
+  } else if(!tall && height < norm) {
+    height += 1;
+  }
+
+  // Handle 3 rise cases up, down, back to normal - JBG
+  if(rise % 3 == 1 && camera.position.y < height/2) {
+    camera.position.y += 1;
+  } else if(rise % 3 == 2 && camera.position.y > -height/2+y) {
+    camera.position.y -= 1;
+  } else if(rise % 3 == 0 && camera.position.y != y) {
+    camera.position.y += camera.position.y > y ? -1 : 1;
+  } 
+
+  // Adjust camera if we are leaving the room - JBG
+  if(camera.position.y > cube.position.y + height / 2) {
+    camera.position.y = cube.position.y + height / 2;
+  } else if(camera.position.y < cube.position.y - height / 2) {
+    camera.position.y = cube.position.y - height / 2;
+  }
+
+  if(camera.position.x > cube.position.x + width / 2) {
+    camera.position.x = cube.position.x + width / 2;
+  } else if(camera.position.x < cube.position.x - width / 2) {
+    camera.position.x = cube.position.x - width / 2;
+  }
+
+  var rot = cube.rotation;
+  scene.remove(cube);
+  if(cylinder)
+    cube = new THREE.Mesh(new THREE.CylinderGeometry( width / 2, width / 2, height, 50 ), new THREE.MeshFaceMaterial( materials ));
+  else 
+    cube = new THREE.Mesh(new THREE.BoxGeometry( width, height, depth ),  new THREE.MeshFaceMaterial( materials ));
+
+//var raycaster = new THREE.Raycaster(camera.position, new THREE.Vector3(camera.position).normalize());
+//var intersects = raycaster.intersectObjects(cube);
+//console.log(intersects);
+  
+  scene.add(cube);
+  cube.rotation.x = rot.x;
+  cube.rotation.y = rot.y;
+  cube.rotation.z = rot.z;
+}
+
---
+++ video.js
@@ -0,0 +1,86 @@
+var isStreaming = false;
+var v = document.getElementById('v');
+var c = document.getElementById('c');
+var con = c.getContext('2d');
+var w = 480;
+var h = 480;
+
+window.addEventListener('DOMContentLoaded', function() {
+  navigator.getUserMedia = (
+    navigator.getUserMedia || 
+    navigator.webkitGetUserMedia || 
+    navigator.mozGetUserMedia || 
+    navigator.msGetUserMedia);
+  if (navigator.getUserMedia) {
+    // Request access to video only
+    navigator.getUserMedia({
+      video: true,
+      audio: false
+    }, function(stream) {
+      var url = window.URL || window.webkitURL;
+      v.src = url ? url.createObjectURL(stream) : stream;
+      v.play();
+    }, function(error) {
+      alert('Something went wrong. (error code ' + error.code + ')');
+      return;
+    });
+  } else {
+    alert('Sorry, the browser you are using doesn\'t support getUserMedia');
+    return;
+  }
+});
+
+v.addEventListener('canplay', function(e) {
+  if (!isStreaming) {
+    // videoWidth isn't always set correctly in all browsers
+    if (v.videoWidth > 0) h = v.videoHeight / (v.videoWidth / w);
+    c.setAttribute('width', w);
+    c.setAttribute('height', h);
+    // Reverse the canvas image
+    con.translate(w, 0);
+    con.scale(-1, 1);
+    isStreaming = true;
+  }
+}, false);
+
+v.addEventListener('play', function() {
+  // Every 33 milliseconds copy the video image to the canvas
+  setInterval(function() {
+    if (v.paused || v.ended) return;
+    con.fillRect(0, 0, w, h);
+    con.drawImage(v, 0, 0, w, h);
+    //goingGrey();
+    chromaKey();
+  }, 33);
+}, false);
+
+var chromaKey = function() {
+  var camData = con.getImageData(0, 0, w, h);
+  //con.drawImage(bg, 0, 0, w, h);
+  //var bgData = con.getImageData(0, 0, w, h);
+  var imgData = con.createImageData(w, h);
+
+  for (i = 0; i < imgData.width * imgData.height * 4; i += 4) {
+    var r = camData.data[i + 0];
+    var g = camData.data[i + 1];
+    var b = camData.data[i + 2];
+    var a = camData.data[i + 3];
+    // compare rgb levels for green and set alphachannel to 0;
+    selectedR = 130;
+    selectedG = 130; 
+    selectedB = 130;
+    if (r >= selectedR && b >= selectedB && g >= selectedG) {
+      imgData.data[i + 0] = 0;//bgData.data[i + 0];
+      imgData.data[i + 1] = 0;//bgData.data[i + 1];
+      imgData.data[i + 2] = 0;//bgData.data[i + 2];
+      imgData.data[i + 3] = 0;//bgData.data[i + 3];
+    } else {
+      imgData.data[i + 0] = r;
+      imgData.data[i + 1] = g;
+      imgData.data[i + 2] = b;
+      imgData.data[i + 3] = a;
+    }
+  }
+  con.putImageData(imgData, 0, 0);
+};
+
