63e2446fdefdf42ddbf64263d4faa074f1849220
JBG <jamesbryangraves@gmail.com>
2014-09-27 20:16:50 +0200

    first for controller functions

--- controllers.js
+++ controllers.js
@@ -6,6 +6,141 @@ var init = function() {
   });
 };
 
+var initHermitController = function() {
+  $('#hermit-button-1').click(function() {
+    if(cube.scale.x == 1) {
+      cube.scale.set(.05, .05, .05);
+      cube.position.set(-5, -25, cube.position.z);
+    } else {
+      cube.scale.set(1, 1, 1);
+    }
+  });
+  $('#hermit-button-2').click(function() {
+    materialStates = [
+      !materialStates[0],
+      !materialStates[1],
+      !materialStates[2],
+      materialStates[3],
+      !materialStates[4],
+      !materialStates[5]
+    ];
+    updateMaterials();
+  });
+  $('#hermit-button-3').click(function() {
+    if($(this).hasClass('active'))
+      addObject('dog', 'objs/BASSET.OBJ', [20, 10, 10], [10, 10, 10]);
+    else
+      removeObject('dog');
+  });
+};
+
+var initHoboController = function() {
+  $('#hobo-button-1').click(function() {
+    if(shake && shakeAmount != 10) {
+      shakeAmount = 10;
+    } else {
+      shakeAmount = 10;
+      shake = !shake;
+      rotate = !shake; 
+      narrow = !narrow;
+    }
+  });
+  $('#hobo-button-2').click(function() {
+    if(shake && shakeAmount != 1) {
+      shakeAmount = 1;
+    } else {
+      shakeAmount = 1;
+      shake = !shake;
+      rotate = !shake; 
+      narrow = !narrow;
+      shorter = !shorter;
+    }
+  });
+};
+
+var initConvenienceController = function() {
+  $('#convenience-controller .button').click(function() {
+    var actives = $('#convenience-controller .button.active').length;
+    if(actives > 0) {
+      max = 1000 * $('#convenience-controller .button.active').length;    
+      shorter = false, tall = true;
+      if(actives == 3) {
+        addObject('stairs', 'objs/stairs.obj', [-20, -20, 0], [.01, .01, .01]);
+      } else {
+        removeObject('stairs');
+      }
+    } else {
+      tall = false;
+    }
+  });
+};
+
+var initCommunalController = function() {
+  $('#communal-button-2').click(function() {
+    materialStates = [
+      !materialStates[0],
+      materialStates[1],
+      materialStates[2],
+      materialStates[3],
+      materialStates[4],
+      materialStates[5]
+    ];
+    updateMaterials();
+  });
+  $('#communal-button-8').click(function() {
+    materialStates = [
+      materialStates[0],
+      materialStates[1],
+      materialStates[2],
+      materialStates[3],
+      materialStates[4],
+      !materialStates[5]
+    ];
+    updateMaterials();
+
+  });
+   $('#communal-button-4').click(function() {
+    materialStates = [
+      materialStates[0],
+      materialStates[1],
+      materialStates[2],
+      materialStates[3],
+      !materialStates[4],
+      materialStates[5]
+    ];
+    updateMaterials();
+  });
+  $('#communal-button-3').click(function() {
+    materialStates = [
+      materialStates[0],
+      !materialStates[1],
+      materialStates[2],
+      materialStates[3],
+      materialStates[4],
+      materialStates[5]
+    ];
+    updateMaterials();
+  });
+  $('#communal-button-5').click(function() {
+    if(yRotationSpeed < 0.008) yRotationSpeed = 0.008;
+    else yRotationSpeed = 0.001;
+    shorter = !shorter;
+  });
+  $('#communal-button-1').click(function() {
+    if(yRotationSpeed < 0.005) yRotationSpeed = 0.005;
+    else yRotationSpeed = 0.001;
+    narrow = !narrow;
+  });
+  $('#communal-button-6').click(function() {
+    if(yRotationSpeed < 0.01) yRotationSpeed = 0.01;
+    else yRotationSpeed = 0.001;
+    narrow = !narrow;
+  });
+  $('#communal-button-7').click(function() {
+    wide = !wide;
+  });
+};
+
 var showController = function(name) {
   $('#controllers .controller').remove();
   $('#x').before(controllers[name + '-controller']);
@@ -15,16 +150,28 @@ var showController = function(name) {
   $('.button').each(function() {
     if($(this).hasClass('sticky')) {
       $(this).mousedown(function() {
+        console.log($(this).attr('id'));
         $(this).toggleClass('active');
       });
     } else {
       $(this).mousedown(function() {
+        console.log($(this).attr('id'));
         $(this).addClass('active');      
       }).mouseup(function() {
         $(this).removeClass('active');
       });
     }
   });
+
+  if(name == 'communal') {
+    initCommunalController();
+  } else if(name == 'convenience') {
+    initConvenienceController();
+  } else if(name == 'hobo') {
+    initHoboController();
+  } else if(name == 'hermit') {
+    initHermitController();
+  }
 };
 
 $(function() {
--- room.js
+++ room.js
@@ -3,29 +3,39 @@ var controls;
 var cube;
 var min = 100, max = 1000, norm = 400;
 var width = norm, height = norm, depth = norm;
+var yRotationSpeed = 0.001;
 
 var maxRise = height/2-20, minRise = -(height/2-30);
 
+var x = 0;
 var y = 25;
 var z = 25;
 
-var narrow = false;
 var tall = false;
+var wide = false;
+var narrow = false;
+var shorter = false;
 var cylinder = false;
 var rise = 0;
+var rotate = true;
+var shake = false;
+var closer = false;
+var shakeCount = 0;
+var shakeAmount = 10;
 
-init();
-animate();
+var materialStates = [false, false, false, false, false, false];
+var materials;
 
-function init() {
+var objs = {};
+
+var init = function() {
   renderer = new THREE.WebGLRenderer();
   renderer.setClearColor( 0xfffa00, 1);
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
-  //
   camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
-  camera.position.x = 0;
+  camera.position.x = x;
   camera.position.y = y;
   camera.position.z = z;
   scene = new THREE.Scene();
@@ -51,7 +61,7 @@ function init() {
     if (e.keyCode == 87) {
       narrow = !narrow;
     } else if(e.keyCode == 72) {
-      tall = !tall;
+      shorter = !shorter;
     } else if(e.keyCode == 67) {
       cylinder = !cylinder;
     } else if(e.keyCode == 82) {
@@ -85,16 +95,7 @@ function init() {
   //var cubeGeo = new THREE.BoxGeometry(200,200,200);
   //cube = new THREE.Mesh(cubeGeo, new THREE.MeshBasicMaterial());
 
-  var img = new Image();
-  img.src = 'Grid.png';
-  var tex = new THREE.Texture(img);
-  img.tex = tex;
-  img.onload = function() {
-    this.tex.needsUpdate = true;
-  };
-  var material = new THREE.MeshBasicMaterial({color: 0xffffff, map: tex});
-
-        
+       
   /*
   materials = [
     //new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true, transparent: true, opacity: 0.1 } ),
@@ -108,38 +109,29 @@ function init() {
     new THREE.MeshBasicMaterial( { color: 0xffffff } )
   ];
   */
-  materials = [
-     //new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide, transparent: true, opacity: 0.0 }),
-     //new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide, transparent: true, opacity: 0.0 }),
-     //new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide, transparent: true, opacity: 0.0 }),
-     new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide }),
-     new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide }),
-     new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide }),
-     new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide }),
-     new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide }),
-     new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide }),
-     //new THREE.MeshBasicMaterial({color: 0xffffff, map: tex, side: THREE.DoubleSide, transparent: true, opacity: 0.0 }),
-  ];
 
+  
   //var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
   //material.side = THREE.BackSide;
   //cube = new THREE.Mesh(new THREE.BoxGeometry(400, 400, 400), material);
+
+  updateMaterials();
+
 
   cube = new THREE.Mesh(new THREE.BoxGeometry( width, height, depth ),  new THREE.MeshFaceMaterial( materials ));
   //cube = new THREE.EdgesHelper( mesh, 0x000000 );
   //cube.material.linewidth = 2;
   //cube = THREE.SceneUtils.createMultiMaterialObject( new THREE.SphereGeometry( 75, 20, 10 ), materials );
-  cube.position.set( 0, y, 0 );
-  scene.add( cube );
+  //scene.add( cube );
 
   /*
   var ambient = new THREE.AmbientLight( 0x999999 );
   scene.add( ambient );
+  */
 
-  var directionalLight = new THREE.DirectionalLight( 0xffeedd );
+  var directionalLight = new THREE.DirectionalLight( 0xffffff );
   directionalLight.position.set( 0, 0, 1 ).normalize();
   scene.add( directionalLight );
-  */
 
   /*
   THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
@@ -162,7 +154,7 @@ function init() {
 
 }
 
-function onWindowResize() {
+var onWindowResize = function() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize( window.innerWidth, window.innerHeight );
@@ -170,16 +162,29 @@ function onWindowResize() {
   renderer.render( scene, camera );
 }
 
-function animate() {
+var animate = function() {
   requestAnimationFrame( animate );
   checkStates();
   //cube.rotation.x += 0.005;
-  cube.rotation.y += 0.001;
   renderer.render( scene, camera );
   controls.update();
 }
 
-function checkStates() {
+var checkStates = function() {
+
+  // Get taller - jbg
+  if(tall && height < max) {
+    height += 1;
+  } else if(!tall && height > norm) {
+    height -= 1;
+  }
+
+  // Get fatter - jbg
+  if(wide && width < max) {
+    width += 1;
+  } else if(!wide && width > norm) {
+    width -= 1;
+  }
 
   // Get skinnier - JBG
   if(narrow && width > min) {
@@ -188,11 +193,18 @@ function checkStates() {
     width += 1;
   }
 
-  // Get shorter - JBG`
-  if(tall && height > min) {
+  // Get shorterer - JBG`
+  if(shorter && height > min) {
     height -= 1;
-  } else if(!tall && height < norm) {
+  } else if(!shorter && height < norm) {
     height += 1;
+  }
+
+  // Get closer - JBG`
+  if(closer && depth > min) {
+    depth -= 1;
+  } else if(!closer && depth < norm) {
+    depth += 1;
   }
 
   // Handle 3 rise cases up, down, back to normal - JBG
@@ -221,12 +233,22 @@ function checkStates() {
   }
 */
 
+  if(rotate) {
+    cube.rotation.y += yRotationSpeed;
+  }
+
+  if(shake) {
+    doShake();
+  }
+
   var rot = cube.rotation;
+  var scale = cube.scale;
+  var position = cube.position;
   scene.remove(cube);
   if(cylinder)
-    cube = new THREE.Mesh(new THREE.CylinderGeometry( width / 2, width / 2, height, 50 ), new THREE.MeshFaceMaterial( materials ));
+    cube = new THREE.Mesh(new THREE.CylinderGeometry(width / 2, width / 2, height, 50), new THREE.MeshFaceMaterial( materials ));
   else 
-    cube = new THREE.Mesh(new THREE.BoxGeometry( width, height, depth ),  new THREE.MeshFaceMaterial( materials ));
+    cube = new THREE.Mesh(new THREE.BoxGeometry(width, height, depth),  new THREE.MeshFaceMaterial( materials ));
 
 //var raycaster = new THREE.Raycaster(camera.position, new THREE.Vector3(camera.position).normalize());
 //var intersects = raycaster.intersectObjects(cube);
@@ -237,5 +259,66 @@ function checkStates() {
   cube.rotation.x = rot.x;
   cube.rotation.y = rot.y;
   cube.rotation.z = rot.z;
+
+  cube.scale.set(scale.x, scale.y, scale.z);
+  cube.position.set(position.x, position.y, position.z);
+
+  for(var key in objs) {
+    obj = objs[key];
+    //obj.rotation.x = rot.x;
+    //obj.rotation.y = rot.y;
+    //obj.rotation.z = rot.z;
+  }
+
+  //console.log('Camera: ' + camera.position.x + ' ' + camera.position.y + ' ' + camera.position.z);
 }
+
+var updateMaterials = function() {
+  var img = new Image();
+  img.src = 'Grid.png';
+  var tex = new THREE.Texture(img);
+  img.tex = tex;
+  img.onload = function() {
+    this.tex.needsUpdate = true;
+  };
+   
+  var opaqueMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, map: tex,  side: THREE.DoubleSide});
+  var transparentMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, map: tex,  side: THREE.DoubleSide, transparent: true, opacity: 0.0});
+  materials = [
+    materialStates[0] ? transparentMaterial : opaqueMaterial,
+    materialStates[1] ? transparentMaterial : opaqueMaterial,
+    materialStates[2] ? transparentMaterial : opaqueMaterial,
+    materialStates[3] ? transparentMaterial : opaqueMaterial,
+    materialStates[4] ? transparentMaterial : opaqueMaterial,
+    materialStates[5] ? transparentMaterial : opaqueMaterial,
+    materialStates[6] ? transparentMaterial : opaqueMaterial
+  ];
+}
+
+var addObject = function(name, objFile, position, scale) {
+  var loader = new THREE.OBJLoader();
+  loader.load(objFile, function (object) {
+    object.position.set(position[0], position[1], position[2]);
+    object.scale.set(scale[0], scale[1], scale[2]);
+    scene.add(object);
+    objs[name] = object;
+  });
+};
+
+var removeObject = function(name) {
+  scene.remove(objs[name]);
+}
+
+var doShake = function() {
+  if(++shakeCount % shakeAmount == 0) {
+    camera.position.x = Math.floor(Math.random() * 2) + 1;
+  } else if(shakeCount % shakeAmount == 1) {
+    camera.position.y = Math.floor(Math.random() * 2) + 1;
+  } else if(shakeCount % shakeAmount == 2) {
+    camera.position.z = Math.floor(Math.random() * 2) + 1;
+  }
+};
+
+init();
+animate();
 
